1bd2e01abb0531c10c528820c751830e37621772
Aaron Minnick <abminnick@gmail.com>
2021-12-06 16:30:06 -0800

    add .cs logic
    Co-Authored-by: Jacob Wilson <jacobleeeugenewilson@gmail.com>
    Co-Authored-by: Zach Wilson <zwilson22495@gmail.com>

--- Models/Queen.cs
+++ Models/Queen.cs
@@ -14,7 +14,18 @@ namespace Queen.Models
 
     public static bool CanAttack(int targetX, int targetY)
     {
-
+      if (_x == targetX || _y == targetY)
+      {
+        return true;
+      }
+      else if (Math.Abs(_x - targetX) == Math.Abs(_y - targetY))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,29 +1,33 @@
 using System;
+using Queen.Models;
 
-public static void Main()
+public class Queen
 {
-  int queenX;
-  int queenY;
-  int targetX;
-  int targetY;
-
-  Console.WriteLine("Where is the queen? (x,y)");
-  string[] queenCoordArray = Console.ReadLine().Split(',');
-  queenX = queenCoordArray[0];
-  queenY = queenCoordArray[1];
-  Queen queen = new Queen(queenX, queenY);
-
-  Console.WriteLine("Where is the target? (x,y)");
-  string[] targetCoordArray = Console.ReadLine().Split(',');
-  targetX = targetCoordArray[0];
-  targetY = targetCoordArray[1];
-
-  if (queen.CanAttack(targetX, targetY))
+  public static void Main()
   {
-    Console.WriteLine("Yes, the queen can attack that.")
-  }
-  else
-  {
-    Console.WriteLine("No, the queen cannot attack that.")
+    int queenX;
+    int queenY;
+    int targetX;
+    int targetY;
+
+    Console.WriteLine("Where is the queen? (x,y)");
+    string[] queenCoordArray = Console.ReadLine().Split(',');
+    queenX = queenCoordArray[0];
+    queenY = queenCoordArray[1];
+    Queen queen = new Queen(queenX, queenY);
+
+    Console.WriteLine("Where is the target? (x,y)");
+    string[] targetCoordArray = Console.ReadLine().Split(',');
+    targetX = targetCoordArray[0];
+    targetY = targetCoordArray[1];
+
+    if (queen.CanAttack(targetX, targetY))
+    {
+      Console.WriteLine("Yes, the queen can attack that.");
+    }
+    else
+    {
+      Console.WriteLine("No, the queen cannot attack that.");
+    }
   }
 }
